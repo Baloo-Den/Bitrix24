7980413adf1368dd770633bb81b86ca7b0f63747
Denis Shpotin <sel-den@inbox.ru>
2025-04-23 07:04:25 +0300

    task-2 изменил поведение FileExceptionHandlerLog

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,5 +1,11 @@
 <?php
-if (file_exists(__DIR__."/../../vendor/autoload.php")) {
+if (file_exists(__DIR__."/../../vendor/autoload.php")) 
+{
     require_once(__DIR__."/../../vendor/autoload.php");
 }
+if (file_exists(__DIR__."/src/autoloader.php")) 
+{
+    require_once(__DIR__."/src/autoloader.php");
+}
+
 //dump(['el'=>'123'],);
---
+++ local/php_interface/src/Otus/Diag/FileExceptionHandlerLogCustom.php
@@ -0,0 +1,21 @@
+<?php 
+namespace Otus\Diag;
+use Bitrix\Main\Diag\ExceptionHandlerFormatter;
+use Bitrix\Main\Diag\FileExceptionHandlerLog;
+
+class FileExceptionHandlerLogCustom extends FileExceptionHandlerLog
+{
+	
+    public function write($exception, $logType)
+	{
+		$text = ExceptionHandlerFormatter::format($exception, false, $this->level);
+		$context = [
+			'type' => static::logTypeToString($logType),
+		];
+
+		$logLevel = static::logTypeToLevel($logType);
+		$message = "Otus: {date} - Host: {host} - {type} - {$text}\n";
+
+		$this->logger->log($logLevel, $message, $context);
+	}
+}
