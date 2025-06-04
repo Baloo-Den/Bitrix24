f5596d926d7c97f9dbee43388b1f5465c044e2a7
Denis Shpotin <sel-den@inbox.ru>
2025-06-04 15:03:25 +0300

    Синхронизация изменений

--- local/php_interface/event_handler.php
+++ local/php_interface/event_handler.php
@@ -1,7 +1,12 @@
 <?php
 use Bitrix\Main;
+
 $eventManager = Main\EventManager::getInstance();
 
 //Вешаем обработчик на событие создания списка пользовательских свойств OnUserTypeBuildList
 $eventManager->addEventHandler('iblock', 'OnIBlockPropertyBuildList', ['Otus\UserType\CUserTypeTimesheet', 'GetUserTypeDescription']);
+
+$eventManager->addEventHandler("crm", "OnAfterCrmDealAdd", ['Otus\SynchronizationEvents\Synchronization', 'OnAfterDealAdd']);
+$eventManager->addEventHandler("crm", "OnAfterCrmDealUpdate", ['Otus\SynchronizationEvents\Synchronization', 'OnAfterDealUpdate']);
+$eventManager->addEventHandler("iblock", "OnAfterIBlockElementUpdate", ['Otus\SynchronizationEvents\Synchronization', 'OnElementAfterUpdate']);
 
