18d5f1fcda8179f52d63a59306fe732d7c1e4b52
Denis Shpotin <sel-den@inbox.ru>
2025-06-30 10:11:54 +0300

    Локальное приложение, обновляющие поле «Дата последней коммуникации», если добавили новое дело.

--- otus_rest/CRest/settings.php
+++ otus_rest/CRest/settings.php
@@ -1,6 +1,6 @@
 <?php
-define('C_REST_CLIENT_ID','local.685e2239655d31.95747760');//Application ID
-define('C_REST_CLIENT_SECRET','9GJF5WAzauy8wc577chLkX9iJJQCvaLcUT4beomemUXcdE5tE8');//Application key
+define('C_REST_CLIENT_ID','local.686219ffbec061.53238749');//Application ID
+define('C_REST_CLIENT_SECRET','Hi4WE01qgiBaG1b8vVYqMau1fnHBAWpXOLB8Rk17DowY3x2VDf');//Application key
 // or
 //define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
 
--- otus_rest/handler.php
+++ otus_rest/handler.php
@@ -1,15 +1,23 @@
 <?php
-//require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
-//require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 require_once(__DIR__ . '/CRest/crest.php');
-/*CEventLog::Add(array(
-         "SEVERITY" => "SECURITY",
-         "AUDIT_TYPE_ID" => "MY_OWN_TYPE",
-         "MODULE_ID" => "main",
-         "ITEM_ID" => 123,
-         "DESCRIPTION" => 1111111,//var_dump($_REQUEST),
-      ));*/
-file_put_contents('log.log', '1234');
-//\Bitrix\Main\Diag\Debug::dumpToFile($_REQUEST,'Var','/test.log');
 
-//($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+//CRest::setLog($_REQUEST);
+if (isset($_REQUEST['event']) && $_REQUEST['event'] === 'ONCRMACTIVITYADD')//Проверяем подписку на событие 
+{
+    $activityId = $_REQUEST['data']['FIELDS']['ID'];//Выцепляем айдишник активити
+    $activity = CRest::call('crm.activity.get', ['id' => $activityId]);
+    if ($activity && isset($activity['result']['OWNER_ID']))
+     {
+        $contactId = $activity['result']['OWNER_ID'];//Выцепляем айдишник контакта
+        $ownerType = $activity['result']['OWNER_TYPE_ID'];//Выцепляем айдишник сущности
+
+        if ($ownerType == 3) //Если соответствует контактам
+        {
+            $currentDate = date('Y-m-d H:i:s');
+            $updateResult = CRest::call('crm.contact.update', [
+                'id' => $contactId,
+                'fields' => ['UF_CRM_1751258998202' => $currentDate]
+            ]);//Обновляем поле «Дата последней коммуникации»
+        }
+    }
+}
