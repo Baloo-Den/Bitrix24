60156e994d26e674a1e1c89d8fbf596ff0938279
Denis Shpotin <sel-den@inbox.ru>
2025-07-02 07:27:41 +0300

    Собственные REST методы

--- local/php_interface/event_handler.php
+++ local/php_interface/event_handler.php
@@ -6,6 +6,9 @@ $eventManager = Main\EventManager::getInstance();
 //Вешаем обработчик на событие создания списка пользовательских свойств OnUserTypeBuildList
 $eventManager->addEventHandler('iblock', 'OnIBlockPropertyBuildList', ['Otus\UserType\CUserTypeTimesheet', 'GetUserTypeDescription']);
 
+//Новые RESTы
+$eventManager->addEventHandlerCompatible('rest', 'OnRestServiceBuildDescription', ['Otus\NewRest\Events', 'OnRestServiceBuildDescriptionHandler']);
+
 $eventManager->addEventHandler("crm", "OnAfterCrmDealAdd", ['Otus\SynchronizationEvents\Synchronization', 'OnAfterDealAdd']);
 $eventManager->addEventHandler("crm", "OnAfterCrmDealUpdate", ['Otus\SynchronizationEvents\Synchronization', 'OnAfterDealUpdate']);
 $eventManager->addEventHandler("iblock", "OnAfterIBlockElementUpdate", ['Otus\SynchronizationEvents\Synchronization', 'OnElementAfterUpdate']);
